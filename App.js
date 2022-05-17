490bb8879192fed565d4e68e13c5cace76f0c4be
Siddhartha <siddpurwar@gmail.com>
2022-05-17 21:33:13 +0530

    project structure

---
+++ App.js
@@ -0,0 +1,11 @@
+import Main from "./components/Main.js"
+import Navbar from "./components/Navbar.js"
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
